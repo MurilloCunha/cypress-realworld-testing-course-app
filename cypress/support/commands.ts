14a49190d3cc62a25b3853aa4287eaa2e344c3d1
MurilloCunha <62815895+MurilloCunha@users.noreply.github.com>
2025-02-17 10:58:33 -0300

    fix: custom command types

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,3 +3,9 @@
 Cypress.Commands.add("getByData", (selector) => {
   return cy.get(`[data-test=${selector}]`)
 })
+
+declare namespace Cypress {
+  interface Chainable {
+    getByData(dataAttribute: string): Chainable
+  }
+}
