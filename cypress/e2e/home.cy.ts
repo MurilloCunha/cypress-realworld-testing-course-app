2851f2ee5f060f6acd1a58263526530a5960d31e
MurilloCunha <62815895+MurilloCunha@users.noreply.github.com>
2025-02-17 10:03:32 -0300

    test: home page e2e tests

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,16 @@
+describe("home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("contains correct hero heading", () => {
+    cy.get("[data-test='hero-heading']").contains(
+      "Testing Next.js Applications with Cypress"
+    )
+  })
+
+  it("contains correct feature list", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+  })
+})
